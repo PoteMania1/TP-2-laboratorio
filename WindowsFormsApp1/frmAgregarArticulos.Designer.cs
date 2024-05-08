fcd28b8250fe5639403a345cbb3e75ac52c87699
mauricio.villalba@alumnos.frgp.utn.edu.ar <mauricio.villalba@alumnos.frgp.utn.edu.ar>
2024-05-07 23:02:24 -0300

    m

---
+++ WindowsFormsApp1/frmAgregarArticulos.Designer.cs
@@ -0,0 +1,58 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class frmAgregarArticulos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmAgregarArticulos
+            // 
+            this.ClientSize = new System.Drawing.Size(388, 443);
+            this.Name = "frmAgregarArticulos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
--- WindowsFormsApp1/frmArticulo.Designer.cs
+++ WindowsFormsApp1/frmArticulo.Designer.cs
@@ -30,22 +30,33 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.dgv_articulo = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_articulo)).BeginInit();
             this.SuspendLayout();
             // 
             // dgv_articulo
             // 
             this.dgv_articulo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_articulo.Location = new System.Drawing.Point(41, 50);
+            this.dgv_articulo.Location = new System.Drawing.Point(41, 73);
             this.dgv_articulo.Name = "dgv_articulo";
-            this.dgv_articulo.Size = new System.Drawing.Size(452, 263);
+            this.dgv_articulo.Size = new System.Drawing.Size(543, 263);
             this.dgv_articulo.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(41, 358);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
             // 
             // Catalogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(877, 402);
+            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.dgv_articulo);
             this.Name = "Catalogo";
             this.Text = "Catalogo";
@@ -58,6 +69,7 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.DataGridView dgv_articulo;
+        private System.Windows.Forms.Button btnAgregar;
     }
 }
 
