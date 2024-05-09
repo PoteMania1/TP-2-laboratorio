7e335d2bdd56f9ad39347383c7c176529a8dec06
mauricio.villalba@alumnos.frgp.utn.edu.ar <mauricio.villalba@alumnos.frgp.utn.edu.ar>
2024-05-09 20:17:27 -0300

    negocio marca/cat

---
+++ negocio/NegocioMarca.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using acceso_a_datos;
+
+
+namespace negocio
+{
+    public class NegocioMarca
+    {
+        public List<Marca> Listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("SELECT Id, Descripcion FROM MARCAS");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+
+        }
+    }
+}
